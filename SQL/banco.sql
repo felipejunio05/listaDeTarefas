b6fdbb4b85316b8153b08ad0edc5620d7eaae4de
Felipe Junio <felipe_ti@outlook.com>
2020-09-18 17:37:27 -0300

    Definiçao de classes para manipulaçao do banco de dados

--- SQL/banco.sql
+++ SQL/banco.sql
@@ -1,14 +1,19 @@
+--Criando base de Dados 
 CREATE DATABASE IF NOT EXISTS app_listaTaferas;
 
+--Selecionando a base de Dados
 use app_listaTaferas;
 
+--Criando a tabela status
 CREATE TABLE IF NOT EXISTS tb_status (
 	id INT UNSIGNED PRIMARY  KEY AUTO_INCREMENT,
 	status VARCHAR(25) not null
 );
 
+--Preenchendo tabela com os registro 'pendente, realizado'
 INSERT INTO tb_status(status) VALUES ('pendente'), ('realizado');
 
+--criando tabela tarefas
 CREATE TABLE IF NOT EXISTS tb_tarefas (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	id_status INT UNSIGNED  NOT NULL DEFAULT 1,
@@ -17,9 +22,15 @@ CREATE TABLE IF NOT EXISTS tb_tarefas (
 	data_cadastrado DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- criando tabela de usuários
 CREATE TABLE IF NOT EXISTS tb_usuarios (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	senha VARCHAR(32) NOT NULL
 );
+
+--Criando usuário de acesso a base dados com todos privilégios.
+CREATE USER 'listaTarefas'@'localhost' IDENTIFIED BY 't@X3l.#$';
+GRANT ALL PRIVILEGES ON app_listaTarefas.* TO 'listaTarefas'@'localhost';
+FLUSH PRIVILEGES;
---
+++ includes/conexao.php
@@ -0,0 +1,17 @@
+<?php 
+    class Conexao {
+        private $host = 'localhost';
+        private $dbname = 'app_listaTarefas';
+        private $user = 'listaTarefas';
+        private $pass = 't@X3l.#$';
+
+        public function conectar() {
+            try {
+                return new PDO("mysql:host=$this->host;dbname=$this->dbname", "$this->user", "$this->pass");
+
+            } catch(PDOException $error) {
+                echo '<p>'.$error->getMessege().'</p>';
+            }
+        }
+    }
+?>
